a90d32c067f1809b5b98eb402a327aea9506a6ef
Lukas <marxlukas@googlemail.com>
2023-05-01 15:10:26 +0200

    fix: optimized seo stuff feat: added ~500 more posts

--- src/lib/utils/durationFormat.ts
+++
@@ -1,8 +0,0 @@
-import { formatDistance } from "date-fns";
-import { parse, toSeconds } from "iso8601-duration";
-
-export function durationFormat(duration: string) {
-  return formatDistance(0, toSeconds(parse(duration)) * 1000, {
-    includeSeconds: true,
-  });
-}
